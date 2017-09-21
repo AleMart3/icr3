67f9b86873d8b157983109eb5f29fbbda413ee87
Alex3 <Alex3@DESKTOP-ALEX.homenet.telecomitalia.it>
2017-09-21 12:31:28 +0200

    prova

--- src/main/java/it/uniroma3/icr/IcrApplication.java
+++ src/main/java/it/uniroma3/icr/IcrApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class IcrApplication extends SpringBootServletInitializer{
-
+//
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(IcrApplication.class, args);
 	
